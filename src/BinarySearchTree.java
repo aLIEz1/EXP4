a36ad9b3c1524b717063e2d987ccaf347e517377
lm <super1742720898@gmail.com>
2020-05-25 14:50:27 +0800

    :arrow_up: 二叉排序树

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,13 +1,58 @@
+import java.util.Scanner;
+
 public class BinarySearchTree {
-    Tree T;
+    int data;
+    BinarySearchTree lChild;
+    BinarySearchTree rChild;
+
+    public static void main(String[] args) {
+        BinarySearchTree bT = new BinarySearchTree();
+        Scanner sc = new Scanner(System.in);
+        int data = sc.nextInt();
+        bT.data=data;
+        while (true) {
+            data = sc.nextInt();
+            if (data == -1) {
+                bT.PreOrderTree(bT);
+                break;
+            }
+            bT.CreatTree(data);
+        }
+
+    }
+
+    public void Creat(BinarySearchTree bT) {
+        if (bT == null) {
+            return;
+        }
+        if (bT.data < this.data) {
+            if (this.lChild == null) {
+                this.lChild = bT;
+            } else {
+                this.lChild.Creat(bT);
+            }
+        } else {
+            if (this.rChild == null) {
+                this.rChild = bT;
+            } else {
+                this.rChild.Creat(bT);
+            }
+        }
+    }
 
     public void CreatTree(int data) {
-        Tree temp = new Tree();
-        temp.data = data;
-        if (T == null) {
-            T = temp;
+        if (data != -1) {
+            BinarySearchTree temp = new BinarySearchTree();
+            temp.data = data;
+            Creat(temp);
         }
-        Tree cur = T;
-        
+    }
+
+    public void PreOrderTree(BinarySearchTree bT) {
+        if (bT != null) {
+            System.out.print(bT.data + " ");
+            PreOrderTree(bT.lChild);
+            PreOrderTree(bT.rChild);
+        }
     }
 }
--- src/Tree.java
+++
@@ -1,5 +0,0 @@
-public class Tree {
-    int data;
-    Tree lChild;
-    Tree rChild;
-}
