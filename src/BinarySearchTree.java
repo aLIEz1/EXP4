fec1ebbe5e160f23fd212c518a12cdda791c01ba
lm <super1742720898@gmail.com>
2020-05-25 16:39:45 +0800

    :arrow_up: MinHeightSearchTree

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class BinarySearchTree {
@@ -7,17 +9,44 @@ public class BinarySearchTree {
 
     public static void main(String[] args) {
         BinarySearchTree bT = new BinarySearchTree();
+        bT.lChild = new BinarySearchTree();
+        bT.rChild = new BinarySearchTree();
         Scanner sc = new Scanner(System.in);
-        int data = sc.nextInt();
-        bT.data=data;
+        int data;
+        List<Integer> list = new ArrayList<>();
+        List<Integer> listFormer = new ArrayList<>();
+        List<Integer> listLatter = new ArrayList<>();
         while (true) {
             data = sc.nextInt();
             if (data == -1) {
-                bT.PreOrderTree(bT);
                 break;
+            } else {
+                list.add(data);
             }
-            bT.CreatTree(data);
         }
+        list.sort((o1, o2) -> o2 - o1);
+        bT.data = list.get(list.size() / 2);
+//        System.out.println(bT.data);
+        for (int i = 0; i < (list.size() / 2); i++) {
+            listFormer.add(list.get(i));
+        }
+        for (int i = (list.size() / 2) + 1; i < list.size(); i++) {
+            listLatter.add(list.get(i));
+        }
+//        for (Integer i:list){
+//            System.out.print("ALL"+i+" ");
+//        }
+//        System.out.println();
+
+        for (Integer integer : listFormer) {
+//            System.out.print("LEFT"+i+" ");
+            bT.lChild.CreatTree(integer);
+        }
+        for (Integer integer : listLatter) {
+//            System.out.print("RIGHT"+i+" ");
+            bT.rChild.CreatTree(integer);
+        }
+        System.out.print(bT.calcHeight(bT) - 1);
 
     }
 
@@ -54,5 +83,14 @@ public class BinarySearchTree {
             PreOrderTree(bT.lChild);
             PreOrderTree(bT.rChild);
         }
+    }
+
+    public int calcHeight(BinarySearchTree b) {
+        if (b == null) {
+            return 0;
+        } else {
+            return Math.max(calcHeight(b.lChild), calcHeight(b.rChild)) + 1;
+        }
+
     }
 }
